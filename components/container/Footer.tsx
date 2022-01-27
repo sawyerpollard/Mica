c7a7128cd056a31d12de626c5afb998f59dd4839
Sawyer Pollard <19192015+sawyerpollard@users.noreply.github.com>
2022-01-26 22:28:40 -0500

    Update footer styling and temporarily hide copyright text

--- components/container/Footer.tsx
+++ components/container/Footer.tsx
@@ -11,13 +11,19 @@ const socialUrls = [
 
 export default function Footer() {
     return (
-        <footer className="text-white bg-purple-900 px-16 py-4 mt-10 whitespace-nowrap">
-            <section className="grid gap-6 grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-                <div className="flex flex-col justify-center gap-y-4 items-center row-span-3">
+        <footer className="text-white bg-purple-900 mt-10 whitespace-nowrap">
+            <section className="grid gap-x-10 grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+                <div className="flex flex-col justify-center gap-y-4 p-6 items-center row-span-3 overflow-hidden bg-black/20">
                     <p className="text-2xl font-logo">The Amherst Student</p>
+                    <p className="font-serif">Established 1868</p>
                     <SocialIcons urls={socialUrls} />
-
                     <DonateButton url={process.env.GHOST_URL + '/donate'} />
+                    <a
+                        className="font-serif font-light text-blue-300 hover:text-blue-300/80 transition"
+                        href="mailto:astudent@amherst.edu"
+                    >
+                        astudent@amherst.edu
+                    </a>
                 </div>
                 <FooterLinkList links={CategoryLinks.news} />
                 <FooterLinkList links={CategoryLinks.opinion} />
@@ -26,8 +32,8 @@ export default function Footer() {
                 <FooterLinkList links={CategoryLinks.podcasts} />
                 <FooterLinkList links={CategoryLinks.amusements} />
             </section>
-            <p className="text-xs self-end font-medium mt-4 tracking-wide uppercase block text-center">
-                © {new Date().getFullYear()} The Amherst Student
+            <p className="text-xs font-serif font-light text-white/60 hidden tracking-wide text-center">
+                © {new Date().getFullYear()} The Amherst Student. All Rights Reserved.
             </p>
         </footer>
     );
@@ -47,13 +53,13 @@ function FooterLinkList({
                 key={link.url}
                 className="text-center md:text-left md:block first:block first:text-2xl first:font-semibold first:mb-2 first:font-serif font-light text-md hidden"
             >
-                <a className="py-1 block hover:text-white/80 transition" href={link.url} key={link.url}>
+                <a className="py-2 block hover:text-white/80 transition" href={link.url} key={link.url}>
                     {link.name}
                 </a>
             </li>
         );
     });
-    return <ul className="">{items}</ul>;
+    return <ul className="py-2 md:py-6">{items}</ul>;
 }
 
 function SocialIcons({ urls }: { urls: string[] }) {
