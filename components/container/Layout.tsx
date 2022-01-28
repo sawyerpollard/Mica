141ca5902afb4add60c3f5be9925fbbbe4f59042
Sawyer Pollard <19192015+sawyerpollard@users.noreply.github.com>
2022-01-27 23:17:57 -0500

    Remove unused import

--- components/container/Layout.tsx
+++ components/container/Layout.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import Footer from './Footer';
 import Header from './Header';
-import Navbar from './Navbar';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
