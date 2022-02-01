c63f8687a23949c6c3271e40e091f60dfdfe5a56
Sawyer Pollard <19192015+sawyerpollard@users.noreply.github.com>
2022-02-01 18:23:42 -0500

    Create left and right topbar links

--- components/container/Topbar.tsx
+++ components/container/Topbar.tsx
@@ -3,17 +3,30 @@ import { OpenWeatherInfo } from '../OpenWeatherInfo';
 import TopbarLinks from '../../lib/TopbarLinks';
 
 export default function Topbar() {
-    const items = TopbarLinks.map((topbarLink) => (
-        <a key={topbarLink.url} className="p-2 hover:text-purple-900 transition" href={topbarLink.url}>
+    const leftItems = TopbarLinks.left.map((topbarLink) => (
+        <a
+            key={topbarLink.url}
+            className="inline-block px-2 py-0.5 hover:text-purple-900 transition"
+            href={topbarLink.url}
+        >
             {topbarLink.name}
         </a>
     ));
+
+    const rightItems = TopbarLinks.right.map((topbarLink) => (
+        <a
+            key={topbarLink.url}
+            className="inline-block px-2 py-0.5 hover:text-purple-900 transition"
+            href={topbarLink.url}
+        >
+            {topbarLink.name}
+        </a>
+    ));
+
     return (
-        <section className="hidden md:flex justify-between items-center bg-gray-100 drop-shadow text-gray-500 text-light text-sm">
-            <div>{items}</div>
-            <div className="px-2 h-8">
-                <OpenWeatherInfo cityId={4929023} />
-            </div>
+        <section className="hidden md:flex justify-between items-center bg-white drop-shadow text-gray-400 text-light text-sm whitespace-nowrap">
+            <div>{leftItems}</div>
+            <div>{rightItems}</div>
         </section>
     );
 }
--- lib/TopbarLinks.ts
+++ lib/TopbarLinks.ts
@@ -1,18 +1,46 @@
-export default [
-    {
-        name: 'About',
-        url: process.env.GHOST_URL + '/about',
-    },
-    {
-        name: 'Advertise',
-        url: process.env.GHOST_URL + '/advertise',
-    },
-    {
-        name: 'Contribute',
-        url: process.env.GHOST_URL + '/contribute',
-    },
-    {
-        name: 'Subscribe',
-        url: process.env.GHOST_URL + '/subscribe',
-    },
-];
+export default {
+    left: [
+        {
+            name: 'About',
+            url: process.env.GHOST_URL + '/about',
+        },
+        {
+            name: 'Masthead',
+            url: process.env.GHOST_URL + '/masthead',
+        },
+        {
+            name: 'Subscribe',
+            url: process.env.GHOST_URL + '/subscribe',
+        },
+        {
+            name: 'Donate',
+            url: process.env.GHOST_URL + '/donate',
+        },
+        {
+            name: 'Advertise',
+            url: process.env.GHOST_URL + '/advertise',
+        },
+        {
+            name: 'Contact',
+            url: process.env.GHOST_URL + '/contact',
+        },
+    ],
+    right: [
+        {
+            name: 'Feedback',
+            url: process.env.GHOST_URL + '/about',
+        },
+        {
+            name: 'Letters to the Editor',
+            url: process.env.GHOST_URL + '/advertise',
+        },
+        {
+            name: 'News Tips',
+            url: process.env.GHOST_URL + '/contribute',
+        },
+        {
+            name: 'Join Our Staff',
+            url: process.env.GHOST_URL + '/subscribe',
+        },
+    ],
+};
