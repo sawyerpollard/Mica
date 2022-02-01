ee2ed2b9012f04fc9710d3f35b70c9c0876b7114
Sawyer Pollard <19192015+sawyerpollard@users.noreply.github.com>
2022-02-01 18:25:07 -0500

    Add issue and volume number to header and adjust layout

--- components/container/Header.tsx
+++ components/container/Header.tsx
@@ -1,4 +1,9 @@
 import NavbarLinks from '../../lib/NavbarLinks';
+import SocialLinks from '../../lib/SocialLinks';
+
+import { OpenWeatherInfo } from '../OpenWeatherInfo';
+
+import { SocialIcon } from 'react-social-icons';
 
 import Navbar from './Navbar';
 import Topbar from './Topbar';
@@ -8,17 +13,47 @@ export default function Header() {
         <>
             <header>
                 <Topbar />
-                <div className="flex flex-col items-center my-5">
-                    <h1 className="text-4xl md:text-7xl lg:text-8xl whitespace-nowrap font-logo">
-                        The Amherst Student
-                    </h1>
-                    <p className="hidden font-medium md:inline">{naturalDateString(new Date())}</p>
-                </div>
+                <section className="flex justify-between items-start">
+                    <div>
+                        <SocialIcons urls={SocialLinks} />
+                    </div>
+                    <div className="flex flex-col items-center my-5">
+                        <h1 className="text-4xl md:text-7xl lg:text-8xl whitespace-nowrap font-logo">
+                            The Amherst Student
+                        </h1>
+                        <p className="hidden font-light italic md:inline">
+                            {naturalDateString(new Date())} • {issueString(151, 13)}
+                        </p>
+                    </div>
+                    <div className="px-3 py-0.5">
+                        <OpenWeatherInfo cityId={4929023} />
+                    </div>
+                </section>
             </header>
             <div className="sticky z-50 top-0 mb-6">
                 <Navbar navbarLinks={NavbarLinks} />
             </div>
         </>
+    );
+}
+
+function issueString(volume: number, issue: number): string {
+    return `Volume ${volume}, Issue ${issue}`;
+}
+
+function SocialIcons({ urls }: { urls: string[] }) {
+    return (
+        <div className="flex">
+            {urls.map((url) => (
+                <SocialIcon
+                    key={url}
+                    style={{ height: 40, width: 40 }}
+                    fgColor="black"
+                    bgColor="transparent"
+                    url={url}
+                />
+            ))}
+        </div>
     );
 }
 
