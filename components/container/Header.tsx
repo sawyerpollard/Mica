93fbdaeffefb754f6c64607bdabb3bce315f1a28
Sawyer Pollard <19192015+sawyerpollard@users.noreply.github.com>
2022-03-30 18:52:42 -0400

    Update issue number

--- components/container/Header.tsx
+++ components/container/Header.tsx
@@ -14,7 +14,7 @@ export default function Header() {
                 <section className="flex md:justify-around">
                     <div className="hidden lg:flex items-center justify-center">
                         <p className="hidden uppercase font-bold md:inline">
-                            {issueString(151, 15)}
+                            {issueString(151, 21)}
                         </p>
                     </div>
                     <div className="hidden md:flex flex-col items-center my-6">
