68c41917f882be06a5628d3e18a2974372a1716f
Sawyer Pollard <19192015+sawyerpollard@users.noreply.github.com>
2022-09-15 01:47:11 -0400

    Update author page

--- pages/author/[slug].tsx
+++ pages/author/[slug].tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps, GetStaticPaths } from 'next';
 import type { Author, PostsOrPages } from '@tryghost/content-api';
 
 import { getGhostAuthor, getGhostAuthors, getGhostPosts } from '../../lib/GetGhost';
+import { naturalDateString } from '../../components/PublishDate';
 
 import WideArticleList from '../../components/WideArticleList';
 
@@ -9,17 +10,23 @@ export default function AuthorPage(props: {
     author: Author;
     articles: PostsOrPages;
 }) {
+    const firstArticle = props.articles[props.articles.length - 1];
+    const lastArticle = props.articles[0];
+
     return (
-        <div className="mx-auto container p-8">
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-                <div className="flex flex-col gap-8 col-span-1">
-                    <h1 className="font-serif text-5xl text-purple-900 font-bold">{props.author.name}</h1>
-                    {props.author.bio && <p>{props.author.bio}</p>}
+        <div className="mx-auto container max-w-screen-lg p-8 flex flex-col gap-16">
+            <div className="flex justify-between items-center">
+                <div className="flex flex-col gap-4">
+                    <h1 className="font-serif text-5xl lg:text-6xl font-extrabold whitespace-nowrap">{props.author.name}</h1>
+                    {props.author.bio && <p className="max-w-prose">{props.author.bio}</p>}
                 </div>
-                <div className="col-span-2">
-                    <WideArticleList articles={props.articles} showDate />
+                <div className="hidden lg:flex flex-col gap-2 whitespace-nowrap text-sm text-right">
+                    <p>Number of Articles: {props.articles.length}</p>
+                    {firstArticle && firstArticle.published_at && <p>First Article: {naturalDateString(new Date(firstArticle.published_at))}</p>}
+                    {lastArticle && lastArticle.published_at && <p>Latest Article: {naturalDateString(new Date(lastArticle.published_at))}</p>}
                 </div>
             </div>
+            <WideArticleList articles={props.articles} showImages showDate />
         </div>
     );
 }
@@ -33,7 +40,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
         };
     }
 
-    const articles = await getGhostPosts({ filter: `author:${author.slug}`, include: 'authors'});
+    const articles = await getGhostPosts({ limit: 'all', filter: `author:${author.slug}`, include: 'authors' });
 
     return {
         props: {
